84f2561e7cb959c56e8a0222331ac4c1003339ac
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2023-11-07 15:10:21 +0900

    inputでエラー

--- Input.cpp
+++ Input.cpp
@@ -4,27 +4,27 @@
 
 using namespace Microsoft::WRL;
 
-//void Input::Initialize(WinApp* winApp)
-//{
-//    winApp_ = winApp;
-//    HRESULT result;
-//
-//    // DirectInputの初期化
-// 
-//    result = DirectInput8Create(
-//        winApp_->GetHInstance(), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
-//    assert(SUCCEEDED(result));
-//
-//    // キーボードデバイスの生成
-//    result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
-//    // 入力データ形式のセット
-//    result = keyboard->SetDataFormat(&c_dfDIKeyboard); // 標準形式
-//    assert(SUCCEEDED(result));
-//    // 排他制御レベルのセット
-//    result = keyboard->SetCooperativeLevel(
-//        winApp_->GetHwnd(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
-//    assert(SUCCEEDED(result));
-//}
+void Input::Initialize(WinApp* winApp)
+{
+    winApp_ = winApp;
+    HRESULT result;
+
+    // DirectInputの初期化
+ 
+    result = DirectInput8Create(
+        winApp_->GetHInstance(), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
+    assert(SUCCEEDED(result));
+
+    // キーボードデバイスの生成
+    result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+    // 入力データ形式のセット
+    result = keyboard->SetDataFormat(&c_dfDIKeyboard); // 標準形式
+    assert(SUCCEEDED(result));
+    // 排他制御レベルのセット
+    result = keyboard->SetCooperativeLevel(
+        winApp_->GetHwnd(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+    assert(SUCCEEDED(result));
+}
 
 void Input::Update()
 {
@@ -33,12 +33,12 @@ void Input::Update()
     //}
 
     //keyPreの中にkeyの情報をコピーする
-    //memcpy(keyPre, key, sizeof(key));
+    memcpy(keyPre, key, sizeof(key));
 
-    //// キーボード情報の取得開始
-    //keyboard->Acquire();
-    //// 全キーの入力状態を取得する
-    //keyboard->GetDeviceState(sizeof(key), key);
+    // キーボード情報の取得開始
+    keyboard->Acquire();
+    // 全キーの入力状態を取得する
+    keyboard->GetDeviceState(sizeof(key), key);
 }
 
 bool Input::PushKey(BYTE keyNumber)
