66b4a1d31aa5042123d38bef7ad0586939734fb9
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-02-09 00:58:47 +0900

    三角形の色を赤色に変更

--- Sprite.cpp
+++ Sprite.cpp
@@ -12,38 +12,52 @@ void Sprite::Initialize(DirectXCommon* dxCommon, SpriteCommon* common)
 	dxCommon_ = dxCommon;
 	common_ = common;
 
-	//VertexResource
-	vertexResource = CreateBufferResource(dxCommon_->GetDevice(), sizeof(XMFLOAT4) * 3);
-
-	
-	//リソースの先頭のアドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	//使用するリソースのサイズは頂点３つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(DirectX::XMFLOAT4) * 3;
-	//1頂点あたりのサイズ
-	vertexBufferView.StrideInBytes = sizeof(DirectX::XMFLOAT4);
+	//頂点情報
+	CreateVertex();
+	//色
+	CreateMaterial();
 }
 
 void Sprite::Draw()
 {
-
-	//頂点リソースにデータを書き込む
-	XMFLOAT4* vertexData = nullptr;
-	//書き込むためのアドレスを取得
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	//左下
-	vertexData[0] = { -0.5f,-0.5f,0.0f,1.0f };
-	//上
-	vertexData[1] = { +0.0f,+0.5f,0.0f,1.0f };
-	//右下
-	vertexData[2] = { +0.5f,-0.5f,0.0f,1.0f };
-
 	dxCommon_->GetCommandList()->SetGraphicsRootSignature(common_->GetRootSignature());
 	dxCommon_->GetCommandList()->SetPipelineState(common_->GetPipelineState());
 
+	//頂点情報
 	dxCommon_->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);
 
 	dxCommon_->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
+	//色情報
+	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+
 	dxCommon_->GetCommandList()->DrawInstanced(3, 1, 0, 0);
+}
+
+void Sprite::CreateVertex()
+{
+	//VertexResource
+	vertexResource = CreateBufferResource(dxCommon_->GetDevice(), sizeof(XMFLOAT4) * 3);
+
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	vertexBufferView.SizeInBytes = sizeof(DirectX::XMFLOAT4) * 3;
+	vertexBufferView.StrideInBytes = sizeof(DirectX::XMFLOAT4);
+
+	//頂点情報
+	XMFLOAT4* vertexData = nullptr;
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+
+	vertexData[0] = { -0.5f,-0.5f,0.0f,1.0f };
+	vertexData[1] = { +0.0f,+0.5f,0.0f,1.0f };
+	vertexData[2] = { +0.5f,-0.5f,0.0f,1.0f };
+}
+
+void Sprite::CreateMaterial()
+{
+	materialResource = CreateBufferResource(dxCommon_->GetDevice(), sizeof(XMFLOAT4));
+
+	XMFLOAT4* materialData = nullptr;
+	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+	
+	*materialData = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
 }
