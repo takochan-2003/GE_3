905cad6ccd910737a8ceeff8350ac830f7c89ead
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-02-08 18:38:57 +0900

    DXC初期化完了

--- SpriteCommon.cpp
+++ SpriteCommon.cpp
@@ -1,10 +1,26 @@
 #include "SpriteCommon.h"
 
-//#include<dxcapi.h>
+#include<wrl.h>
+#include<dxcapi.h>
+#include<cassert>
 
-//#pragma comment(lib,"dxcompiler.lib")
+#pragma comment(lib,"dxcompiler.lib")
+
+using namespace Microsoft::WRL;
 
 void SpriteCommon::Initialize()
 {
+	HRESULT result{};
 
+	//DXCèâä˙âª
+	ComPtr<IDxcUtils> dxcUtils;
+	ComPtr<IDxcCompiler3> dxcCompiler;
+	result = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
+	assert(SUCCEEDED(result));
+	result = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+	assert(SUCCEEDED(result));
+
+	ComPtr<IDxcIncludeHandler> includeHandler;
+	result = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+	assert(SUCCEEDED(result));
 }
